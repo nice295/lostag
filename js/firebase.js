d18558f05b744c422d2a3f9f266beb409d0ff40b
Glitch (walnut-pool-peony) <none>
2021-05-23 12:01:42 +0000

    👞👑 Checkpoint ./js/firebase.js:602196/357 ./js/initmap.js:602196/357 ./index.html:602196/246 ./js/map.js:602196/2

--- js/firebase.js
+++ js/firebase.js
@@ -87,19 +87,6 @@ window.addEventListener('load', function () {
     locationRef.set(location3);    
   });
 
-  function initMap() {
-  // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.036 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: uluru,
-  });
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-  });
-}
+
   
 }, false);
