f997dc9d2d00cff5a1a8e7b4e72b1ac09ea6aa01
Glitch (walnut-pool-peony) <none>
2021-05-23 10:43:36 +0000

    🎇🖐 Checkpoint ./js/firebase.js:602196/7172 ./index.html:602196/15

--- js/firebase.js
+++ js/firebase.js
@@ -28,84 +28,52 @@ window.addEventListener('load', function () {
 //     fanRunRef.set(false);
 //   });
 
-//   var fanSongRef = firebase.database().ref('fan/song');
-//   fanSongRef.on('value', function (snapshot) {
-//     console.log(`song value is ${snapshot.val()}`);
 
-//     $('#js-song-play').removeClass('btn-danger btn-secondary');
-//     $('#js-song-stop').removeClass('btn-danger btn-secondary');
-
-//     if (snapshot.val() == true) {
-//       $('#js-song-play').addClass('btn-danger');
-//     }
-//     else {
-//       $('#js-song-stop').addClass('btn-danger');
-//     }
-//   });
-
-//   $('#js-song-play').click(function (){
-//     fanSongRef.set(true);
-//   });
-
-//   $('#js-song-stop').click(function (){
-//     fanSongRef.set(false);
-//   });
-
-  var fanGreetingRef = firebase.database().ref('fan/greeting');
-  fanGreetingRef.on('value', function (snapshot) {
-    console.log(`greeting value is ${snapshot.val()}`);
-
-    $('#js-greeting-hello').removeClass('btn-danger btn-secondary');
-    $('#js-greeting-bye').removeClass('btn-danger btn-secondary');
-    $('#js-greeting-off').removeClass('btn-danger btn-secondary');
-
-    if (snapshot.val() == 'hello') {
-      $('#js-greeting-hello').addClass('btn-danger');
-
-    }
-    else if (snapshot.val() == 'bye') {
-      $('#js-greeting-bye').addClass('btn-danger');
-    }
-    else if (snapshot.val() == 'off') {
-      $('#js-greeting-off').addClass('btn-danger');
-    }
-  });
-
-  $('#js-greeting-hello').click(function (){
-    fanGreetingRef.set("hello");
-  });
-
-  $('#js-greeting-bye').click(function (){
-    fanGreetingRef.set("bye");
-  });
-
-  $('#js-greeting-off').click(function (){
-    fanGreetingRef.set("off");
-  });
+  //유러피안어드벤처 - 37.29140467051707, 127.20018947041493
+  //로스트밸리- 37.29193336025852, 127.20782870376418
+  //판다월드 - 37.293436016625634, 127.20274316522656
+  var location1 = {
+    // name: '유러피안어드벤처',
+    longitude: '37.29140467051707',
+    latitude: '127.20018947041493',
+    mac: 'aa:bb:cc:dd:ee:ff'
+  };
   
+  var location3 = {
+    // name: '로스트밸리',
+    longitude: '37.29193336025852',
+    latitude: '127.20782870376418',
+    mac: 'aa:bb:cc:dd:ee:ff'
+  };
   
-  var locationRef = firebase.database().ref('Location');
+  var location3 = {
+    // name: '판다월드',
+    longitude: '37.293436016625634',
+    latitude: '127.20274316522656',
+    mac: 'aa:bb:cc:dd:ee:ff'
+  };
+  
+  var locationRef = firebase.database().ref('location');
   locationRef.on('value', function (snapshot) {
-    console.log(`location value is ${snapshot.val()}`);
+    console.log(`location value is ${snapshot.val().longitude}`);
 
     $('#js-send-location-1').removeClass('btn-danger btn-secondary');
     $('#js-send-location-2').removeClass('btn-danger btn-secondary');
-    $('#js-send-location-2').removeClass('btn-danger btn-secondary');
+    $('#js-send-location-3').removeClass('btn-danger btn-secondary');
 
-    if (snapshot.val() == 'hello') {
-      $('#js-greeting-hello').addClass('btn-danger');
-
+    if (snapshot.val().longitude == location1.longitude) {
+      $('#js-send-location-1').addClass('btn-danger');
     }
-    else if (snapshot.val() == 'bye') {
-      $('#js-greeting-bye').addClass('btn-danger');
+    else if (snapshot.val().longitude == location2.longitude) {
+      $('#js-send-location-2').addClass('btn-danger');
     }
-    else if (snapshot.val() == 'off') {
-      $('#js-greeting-off').addClass('btn-danger');
+    else if (snapshot.val().longitude == location3.longitude) {
+      $('#js-send-location-3').addClass('btn-danger');
     }
   });
 
   $('#js-send-location-1').click(function (){
-    console.log(`sending location value is ${snapshot.val()}`);
+    console.log(`sending location 1.`);
     locationRef.set({
       mac: "aa:bb:cc:dd:ee:ff",
       longitude: "37.293910792616444",
@@ -113,12 +81,22 @@ window.addEventListener('load', function () {
     });    
   });
 
-//   $('#js-greeting-bye').click(function (){
-//     fanGreetingRef.set("bye");
-//   });
-
-//   $('#js-greeting-off').click(function (){
-//     fanGreetingRef.set("off");
-//   });
+    $('#js-send-location-2').click(function (){
+    console.log(`sending location 2.`);
+    locationRef.set({
+      mac: "aa:bb:cc:dd:ee:ff",
+      longitude: "37.291315742613484",
+      latitude: "127.20249388047245"
+    });    
+  });
+  
+    $('#js-send-location-3').click(function (){
+    console.log(`sending location 3.`);
+    locationRef.set({
+      mac: "aa:bb:cc:dd:ee:ff",
+      longitude: "37.2916881010234",
+      latitude: "127.20780685561947"
+    });    
+  });
 
 }, false);
